13e6ce48da4ce59e87cdef440c5bdc5893279620
alicewww <alicewww@users.noreply.github.com>
2016-07-12 17:34:17 +0800

    update to use shadowsocks-go instead of shadowsocks-libev

--- dfile/ss-run.sh
+++ dfile/ss-run.sh
@@ -8,16 +8,50 @@
 	PORT="$2"
 	PASSWD="$3"
 
+	echo "
+global
+        log /dev/log    local0
+        log /dev/log    local1 notice
+        chroot /var/lib/haproxy
+        user haproxy
+        group haproxy
+        daemon
+
+defaults
+        timeout connect 3s
+        timeout queue 15s
+        timeout server 3600s
+        timeout client 3600s
+
+frontend fe
+        bind 0.0.0.0:10000
+        mode tcp
+        backlog 4096
+        maxconn 50000
+        default_backend be
+
+backend be
+        mode  tcp
+        balance roundrobin
+        server web1 $SERVER:$PORT
+" >/etc/haproxy/haproxy.cfg
+
 	cron
+	/etc/init.d/haproxy start
 	/etc/init.d/privoxy restart
 	/usr/sbin/squid3
 
-	while true
-	do
-		/usr/bin/ss-tunnel -s ${SERVER} -p ${PORT} -b 0.0.0.0   -l 10000 -k ${PASSWD} -m aes-128-cfb -u -v -L ${SERVER}:${PORT} &
-		/usr/bin/ss-local  -s ${SERVER} -p ${PORT} -b 127.0.0.1 -l 10001 -k ${PASSWD} -m aes-128-cfb -u &
-		sleep 1200
-		jobs -l | grep ss- | awk '{print $2}' | xargs kill
-		fg
-	done
+	cd /root/gopath/bin/
+	echo "
+{
+    \"server\":\"$SERVER\",
+    \"server_port\":$PORT,
+    \"local_port\":10001,
+    \"password\":\"$PASSWD\",
+    \"method\": \"aes-256-cfb-auth\",
+    \"timeout\":600
+}
+" > config.json
+
+	./shadowsocks-local -d
 } >>/ss/ss.log 2>&1
